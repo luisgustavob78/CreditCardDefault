54487bc3f9c3a5a89450f0aae69fd5d701c15bc8
luisgustavob78 <luisgustavob78@gmail.com>
2023-08-12 13:06:14 -0300

    update main.py

--- app/main.py
+++ app/main.py
@@ -9,25 +9,33 @@ import csv
 from io import StringIO
 
 
-app = FastAPI(title="Credit card default prediction! Upload your batch")
+app = FastAPI(title="Credit card default prediction! Upload your json batch")
 
 # Represents a batch of wines
-class Batch(BaseModel):
-    batches: List[List[float]]
+class Credit(BaseModel):
+    batches: List[conlist(item_type=float, min_items=24, max_items=24)]
 
-@app.post("/upload_csv/")
-async def upload_csv(file: UploadFile = File(...)):
-    contents = await file.read()
-    contents = contents.decode("utf-8")
 
-    # Parse the CSV content
-    batch_data = Batch(csv_data=contents)
-    dataframe = pd.read_csv(pd.compat.StringIO(batch_data.csv_data))
-
+@app.on_event("startup")
+def load_clf():
     # Load classifier from pickle file
     clf = joblib.load("../app/model.pkl")
 
-    probs = model.predict_proba(batch_data.batches)
+@app.get("/")
+def home():
+    return "Congratulations! Your API is working as expected. This new version allows for batching. Now head over to http://localhost:81/docs"
+
+
+@app.post("/predict")
+def predict(credit: Credit):
+    batches = credit.batches
+    np_batches = np.array(batches)
+    names = pd.read_csv("../inputs/col_names.csv")
+    col_names = names["col_names"].values
+    df_batch = pd.DataFrame(np_batches)
+    df_batch.columns = col_names
+
+    probs = model.predict_proba(df_batch.batches)
     
     thr = 0.55
     pred = ["default" if v > thr else "good payment" for v in probs]
